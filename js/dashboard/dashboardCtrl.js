f73c578fc9085b03c3938082ab89ffbde1fbae1d
rishabhdev <rishabh.dev@fitkids.co.in>
2017-09-16 09:10:47 +0530

    stats

--- js/dashboard/dashboardCtrl.js
+++ js/dashboard/dashboardCtrl.js
@@ -2,6 +2,26 @@
  * Created by rishabhdev on 15/09/17.
  */
 
-angular.module("gitapp").controller('dashboardCtrl',function(apiService){
+angular.module("gitapp").controller('dashboardCtrl',function($scope,apiService,API_URL){
 
+    $scope.API_URL = API_URL;
+    $scope.bucket = {};
+
+    $scope.fetchIssues = function(){
+
+        apiService.getBoundryIssues($scope.bucket.repoName,true)
+            .then(function(res){
+                console.log("OLD",res);
+               return apiService.getBoundryIssues($scope.bucket.repoName,false)
+            })
+            .then(function(res){
+                console.log("NEW",res);
+                return apiService.getIssues({repoName:$scope.bucket.repoName})
+
+            })
+            .then(function(res){
+                console.log(res);
+                $scope.issues = res.data;
+            })
+    }
 });
--- js/services/apiService.js
+++ js/services/apiService.js
@@ -7,12 +7,28 @@ angular.module("gitapp").factory('apiService',function($http,API_URL){
     var apiService = {};
     apiService.getIssues = function(filter){
 
-        var url = API_URL+'issues/';
-       // if(filter.)
+        var url = API_URL+"repos/"+filter.repoName+'/issues';
 
 
-        $http({
-            method:"GET"
+       return  $http({
+            method:"GET",
+            url:url
         })
     }
+
+    apiService.getBoundryIssues = function (repoName, old) {
+        var order;
+        if(old){
+            order = 'asc';
+        }
+        else{
+          order = 'desc';
+        }
+        var url = API_URL+"repos/"+repoName+'/issues?page=1&per_page=1&state=open&sort=created&order='+order;
+        return  $http({
+            method:"GET",
+            url:url
+        })
+    }
+    return apiService;
 });
