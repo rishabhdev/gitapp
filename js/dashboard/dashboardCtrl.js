294a5e08f6908c8836271c2921f32448b5c379b0
rishabhdev <rishabh.dev@fitkids.co.in>
2017-09-16 09:16:40 +0530

    stat

--- js/dashboard/dashboardCtrl.js
+++ js/dashboard/dashboardCtrl.js
@@ -12,10 +12,13 @@ angular.module("gitapp").controller('dashboardCtrl',function($scope,apiService,A
         apiService.getBoundryIssues($scope.bucket.repoName,true)
             .then(function(res){
                 console.log("OLD",res);
+                $scope.oldest = res.data[0];
                return apiService.getBoundryIssues($scope.bucket.repoName,false)
             })
             .then(function(res){
                 console.log("NEW",res);
+                $scope.recent = res.data[0];
+
                 return apiService.getIssues({repoName:$scope.bucket.repoName})
 
             })
